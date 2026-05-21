15b2b844466154f4525172dd98c3e03c501c919b
Tomas Horacek <t.horacek@student.tue.nl>
2026-05-21 21:38:10 +0200

    first DFT implementation

--- final_project.py
+++ final_project.py
@@ -1,4 +1,20 @@
-# This is a test to check if the commit environemnt is working properly. This file will be deleted after the test.
-print("Hello, World!")
-#here i'll try a second commit to see if the commit environment is working properly. This file will be deleted after the test.
-print("Hello, World! Again!")
+# DFT implementation in Python using NumPy
+
+import numpy as np
+
+def dft(x):
+    x = np.asarray(x, dtype=complex)
+    N = x.size
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(-2j * np.pi * k * n / N)
+    return M @ x
+import numpy as np
+
+def dft(x):
+    x = np.asarray(x, dtype=complex)
+    N = x.size
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(-2j * np.pi * k * n / N)
+    return M @ x
