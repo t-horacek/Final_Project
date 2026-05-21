98cdf81995fc94a773d94aa2cb779f45b32a9eac
Tomas Horacek <t.horacek@student.tue.nl>
2026-05-21 21:46:56 +0200

    definition of the DFT function

--- final_project.py
+++ final_project.py
@@ -2,16 +2,7 @@
 
 import numpy as np
 
-def dft(x):
-    x = np.asarray(x, dtype=complex)
-    N = x.size
-    n = np.arange(N)
-    k = n.reshape((N, 1))
-    M = np.exp(-2j * np.pi * k * n / N)
-    return M @ x
-import numpy as np
-
-def dft(x):
+def DFT(x):
     x = np.asarray(x, dtype=complex)
     N = x.size
     n = np.arange(N)
